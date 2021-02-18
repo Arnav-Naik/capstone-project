b994bd2a2f53ae7c73ad30ad638d928e6b640481
Arnav-Naik <73340433+Arnav-Naik@users.noreply.github.com>
2021-02-18 10:58:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var home = document.getElementById("home");
+var importance = document.getElementById("importance");
+var currentstatus = document.getElementById("currentstatus");
+var futurebenifits = document.getElementById("futurebenifits");
+var aboutme = document.getElementById("aboutme");
+
+var Img = document.getElementsByClassName("img")
+var button = document.getElementById("button");
+var button = document.getElementById("button");
+home.onclick=()=>{
+    location.href="./index.html";
+}
+importance.onclick=()=>{
+    location.href="./importance.html";
+}
+currentstatus.onclick=()=>{
+    location.href="./currentstatus.html";
+}
+futurebenifits.onclick=()=>{
+    location.href="./futurebenifits.html";
+}
+aboutme.onclick=()=>{
+    location.href="./aboutme.html";
+}
